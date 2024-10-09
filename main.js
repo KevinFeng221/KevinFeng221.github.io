6879e4f62f99ae58e04f1df41b8ec42a179fa2e6
KevinFeng221 <k1feng@torontomu.ca>
2024-10-09 00:56:03 -0400

    updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let username;
 
 document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").ariaValueMax;
+    username = document.getElementById("myText").value;
     document.getElementById("myH1").textContent = `Hello ${username}`
 }
