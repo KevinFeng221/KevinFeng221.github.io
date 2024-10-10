9eb26989d599622f058b50673ec0e8552ad89941
KevinFeng221 <k1feng@torontomu.ca>
2024-10-09 20:54:02 -0400

    enlarge

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("myText").value;
     document.getElementById("myH1").textContent = `Hello ${username}`
 }
+
+function enlargeImage() {
+    const img = document.getElementById("profilePic");
+    if (img.classList.contains("enlarged")){
+        img.classList.remove("enlarged");
+    }else{
+        img.classList.add("enlarged");
+    }  
+    }
